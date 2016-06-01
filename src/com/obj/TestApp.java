848f47f286c36d6eda45abb4b7ce2161f072369c
kenta_000 <kenta_000@Ken>
2016-06-01 19:48:05 -0400

    added comment

--- src/com/obj/TestApp.java
+++ src/com/obj/TestApp.java
@@ -13,6 +13,8 @@ public class TestApp {;
     
     public static void main(String [] args){
         System.out.println("hey there");
+        //added comment.
+        System.out.println("It's important to know");
     }
     
 }
