a56940905ff8b88de2e6f704adaab0a104549191
kenta_000 <kenta_000@Ken>
2016-06-25 01:04:06 -0400

    ItemManger added to work with list of items.

---
+++ src/com/obj/ItemManager.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.obj;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kenta_000
+ */
+public class ItemManager {
+    
+    private ArrayList<Item> items;
+    
+    public ItemManager(){
+        
+    }
+    
+    public ItemManager(ArrayList<Item> items){
+        
+    }
+    
+    
+    //TODO implement setters and getters.
+    
+}
